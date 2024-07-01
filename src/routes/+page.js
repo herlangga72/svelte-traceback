895ad533ad77830e8eb8ee2ed30faa5007ff242f
Herlangga Yusuf Syailendra <herlangga72@gmail.com>
2024-07-01 15:15:31 +0700

    update to shiki

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { codeToHtml } from "shiki";
+
 export async function load({ params, url }) {
-    let code = url.searchParams.get('code');
-    return { code: code};
+    let code = url.searchParams.get('code') || "";
+    let codeHTml = await codeToHtml(code,{
+        lang:"python",
+        theme:"github-dark",
+    });
+    return { code: codeHTml};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
 <script context="module" lang="ts">
-    import {marked} from 'marked';
     import { page } from '$app/stores';
     import "../app.css";
 </script>
 
 <script lang="ts">
     let code = $page.data.code;
-    let htmlContent = marked(code) || "";
-
-    $: {
-        htmlContent = marked(code);
-    }
 </script>
+<style>
+code {
+	counter-reset: step;
+	counter-increment: step calc(var(--start, 1) - 1);
+}</style>
+
 <div class="text-white w-full h-full">
     <h3 class="p-3 text-2xl font-bold">Error Log</h3>
     <pre class="m-5">
-        <code data-language="python" class="bg-stone-800 p-8 block text-sm">{code}</code>
+        <code>
+            {@html code}
+        </code>
     </pre>
 </div>
